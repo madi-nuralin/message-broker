c5cadbd1a8372853da6303b78711c154c0542286
Madi Nuralin <nmdbzk@gmail.com>
2023-07-27 01:35:56 +0500

    Update message_broker.hpp

--- message_broker.hpp
+++ message_broker.hpp
@@ -9,6 +9,118 @@
 #include <json-glib/json-glib.h>
 #include <json-glib/json-gobject.h>
 
+namespace gammasoft {
+
+class VistaMessageBroker {
+public:
+    VistaMessageBroker(const std::string &host = "127.0.0.1", int port = 5672, const std::string &username = "guest", const std::string &password = "guest", const std::string &vhost = "/", int frame_max = 131072);
+    ~VistaMessageBroker();
+
+    class Statement,
+    class Request,
+    class Response;
+
+//	void publish(amq::exchange &exchange, amq::queue &queue, const std::string &routingkey, const std::string &message);
+	void publish(const std::string &exchange, const std::string &routingkey, const std::string &messagebody);
+//	void publish(amq::exchange &exchange, amq::queue &queue, const std::string &routingkey, const std::string &message, void (*callback)(const Response &response));
+	void publish(const std::string &exchange, const std::string &routingkey, const std::string &messagebody, void (*callback)(const Response &response));
+//	void subscribe(amq::exchange &exchange, amq::queue &queue, const std::string &bindingkey, const std::string &message);
+	void subscribe(const std::string &exchange, const std::string &bindingkey, void (*callback)(const Statement &statement));
+//	void subscribe(amq::exchange &exchange, amq::queue &queue, const std::string &bindingkey, const std::string &message);
+	void subscribe(const std::string &exchange, const std::string &bindingkey, void (*callback)(const Request &request, Response &response));
+
+private:
+	std::string m_host;
+	std::string m_username;
+	std::string m_password;
+	std::string m_vhost;
+	int m_port;
+	int m_frame_max;
+};
+
+namespace amq {
+class envelope : public amqp_envelope_t {
+
+};
+
+class message : public amqp_message_t {
+public:
+	message(const std::string &body) {
+		this->properties._flags = 0;
+		this->body = amqp_bytes_malloc_dup(amqp_cstring_bytes(body.c_str()));
+	}
+
+	void property(const std::string & key, const char *value) {
+		*reinterpret_cast<amqp_bytes_t*>(schema[key].ptr) = amqp_cstring_bytes(value);
+		this->properties._flags |= schema[key].flag;
+	}
+
+	void property(const std::string & key, uint8_t value) {
+		*reinterpret_cast<uint8_t*>(schema[key].ptr) = value;
+		this->properties._flags |= schema[key].flag;
+	}
+
+	void property(const std::string & key, uint64_t value) {
+		*reinterpret_cast<uint64_t*>(schema[key].ptr) = value;
+		this->properties._flags |= schema[key].flag;
+	}
+
+protected:
+	struct PropertyDescriptor {
+		amqp_flags_t flag;
+		void *ptr;
+	};
+
+	std::map<std::string, PropertyDescriptor> schema {
+		{"Content-Type", PropertyDescriptor{AMQP_BASIC_CONTENT_TYPE_FLAG, &properties.content_type}},
+		{"Content-Encoding", PropertyDescriptor{AMQP_BASIC_CONTENT_ENCODING_FLAG, &properties.content_encoding}},
+		{"Delivery-Mode", PropertyDescriptor{AMQP_BASIC_DELIVERY_MODE_FLAG, &properties.delivery_mode}},
+		{"Priority", PropertyDescriptor{AMQP_BASIC_PRIORITY_FLAG, &properties.priority}},
+		{"Correlation-Id", PropertyDescriptor{AMQP_BASIC_CORRELATION_ID_FLAG, &properties.correlation_id}},
+		{"Reply-To", PropertyDescriptor{AMQP_BASIC_REPLY_TO_FLAG, &properties.reply_to}},
+		{"Expiration", PropertyDescriptor{AMQP_BASIC_EXPIRATION_FLAG, &properties.expiration}},
+		{"Message-Id", PropertyDescriptor{AMQP_BASIC_MESSAGE_ID_FLAG, &properties.message_id}},
+		{"Timestamp", PropertyDescriptor{AMQP_BASIC_TIMESTAMP_FLAG, &properties.timestamp}},
+		{"Type", PropertyDescriptor{AMQP_BASIC_TYPE_FLAG, &properties.type}},
+		{"User-Id", PropertyDescriptor{AMQP_BASIC_USER_ID_FLAG, &properties.user_id}},
+		{"App-Id", PropertyDescriptor{AMQP_BASIC_APP_ID_FLAG, &properties.app_id}},
+		{"Cluster-Id", PropertyDescriptor{AMQP_BASIC_CLUSTER_ID_FLAG, &properties.cluster_id}}
+	};
+};
+
+struct exchange {
+	exchange(const std::string &exchange_name, const std::string &exchange_type = "direct", bool passive = false, bool durable = false, bool auto_delete = false, bool internal = false)
+		: name(exchange_name), type(exchange_type), passive(passive), durable(durable), auto_delete(auto_delete), internal(internal) {}
+
+	std::string name, type;
+	bool passive, durable, auto_delete, internal;
+};
+
+struct queue {
+	queue(const std::string &queue_name = "", bool passive = false, bool durable = false, bool exclusive = false, bool auto_delete = true)
+		: name(queue_name), passive(passive), durable(durable), exclusive(exclusive), auto_delete(auto_delete) {}
+
+	std::string name;
+	bool passive, durable, auto_delete, exclusive
+};
+
+class connection {
+public:
+	connection(const std::string &host = "127.0.0.1", int port = 5672, const std::string &username = "guest", const std::string &password = "guest", const std::string &vhost = "/", int frame_max = 131072);
+	~connection();
+
+	amqp_socket_t *socket = NULL;
+	amqp_connection_state_t conn;
+
+	void declare_exchange(amq::exchange &exchange);
+	void declare_queue(amq::queue &queue);
+	void bind_queue(const std::string &queuename, const std::string &exchange, const std::string &routingkey);
+	void basic_publish(const std::string &exchange, const std::string &routingkey, const amq::message &message, bool mandatory = false, bool immediate = false);
+	void basic_consume(const std::string &queuename, const std::string &consumer_tag = "", bool no_local = true, bool no_ack = true, bool exclusive = true, uint16_t message_prefetch_count = 1);
+};
+};
+};
+
 class MessageBroker
 {
 public:
