f4f22897f666c49d15706afbb8b5fb084cbc946d
Madi <nmdbzk@gmail.com>
2023-08-09 20:09:48 +0500

    Update server_main.cpp

--- examples/server_main.cpp
+++ examples/server_main.cpp
@@ -20,21 +20,21 @@ int main(int argc, char const *argv[])
 {
 	MessageBroker broker;
 
-	/*broker.subscribe({
-	    .queue = {.name = "hello", .declare = true}
-	    .on_receive = [](const auto& message){
-		g_message("[x] Received b'%s'", message.getBody().c_str());
-	     });
+	broker.subscribe({
+ 		.queue = {.name = "hello", .declare = true}
+   		.on_receive = [](const auto& message){
+			g_message("[x] Received b'%s'", message.getBody().c_str());
+		});
 
 	broker.subscribe({
-	    .queue = {.name = "rpc_queue", .declare = true},
-	    .message = "10",
-	    .on_receive = [](const auto& request, auto& response){
-		auto n = std::stoi(request.getBody());
-		g_message("[.] fib('%d')", n);
-		response.setBody(std::to_string(fib(n)));
-		return true;
-	    });
+		.queue = {.name = "rpc_queue", .declare = true},
+		.message = "10",
+		.on_receive = [](const auto& request, auto& response){
+			auto n = std::stoi(request.getBody());
+			g_message("[.] fib('%d')", n);
+			response.setBody(std::to_string(fib(n)));
+			return true;
+		});
 
 	while(1);**/
 
