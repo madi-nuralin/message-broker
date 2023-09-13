ed0d03f193f0f54a83e3be42a194112201ff73c6
Madi Nuralin <nmdbzk@gmail.com>
2023-09-14 02:09:53 +0600

    WIP: Refactor: Pimpl idiom

--- message_broker.cpp
+++ message_broker.cpp
@@ -12,202 +12,254 @@ namespace gammasoft {
 
 namespace amqp {
 
+class ConnectionImpl
+{
+	amqp_connection_state_t m_state;
+
+public:
+	inline ConnectionImpl(
+		const std::string &host = "127.0.0.1", int port = 5672,
+		const std::string &username = "guest",
+		const std::string &password = "guest",
+		const std::string &vhost = "/", int frame_max = 131072)
+	{
+		m_state = amqp_new_connection();
+		amqp_socket_t *socket = amqp_tcp_socket_new(m_state);
+		if (!socket) {
+			die("AMQP creating TCP socket failed");
+		}
+		int status = amqp_socket_open(socket, host.c_str(), port);
+		if (status) {
+			die("AMQP opening TCP socket on %s:%d failed", host.c_str(), port);
+		}
+		die_on_amqp_error(amqp_login(m_state, vhost.c_str(), 0, frame_max, 0, AMQP_SASL_METHOD_PLAIN,
+				username.c_str(), password.c_str()),
+			"Logging in");
+	}
+
+	inline virtual ~ConnectionImpl()
+	{
+		die_on_amqp_error(amqp_connection_close(m_state, AMQP_REPLY_SUCCESS),
+			"connection.close");
+		die_on_error(amqp_destroy_connection(m_state),
+			"destroy.connection");
+	}
+
+	void maybe_release_buffers()
+	{
+		amqp_maybe_release_buffers(m_state);
+	}
+
+	RpcReply consume_message(Envelope *envelope, const struct timeval *timeout, int flags)
+	{
+		return amqp_consume_message(m_state, envelope, timeout, flags);
+	}
+
+	friend class ChannelImpl;
+};
+
+class ChannelImpl
+{
+	amqp_channel_t m_id;
+	ConnectionImpl *m_connection;
+
+public:
+	inline ChannelImpl(Connection *connection) : m_id(1)
+	{
+		m_connection = connection->m_impl.get();
+		amqp_channel_open(m_connection->m_state, m_id);
+		die_on_amqp_error(amqp_get_rpc_reply(m_connection->m_state),
+			"channel.open");
+	}
+
+	inline virtual ~ChannelImpl()
+	{
+		die_on_amqp_error(amqp_channel_close(m_connection->m_state, m_id, AMQP_REPLY_SUCCESS),
+			"channel.close");
+	}
+
+	std::tuple<std::string, std::string> setup(const Configuration& configuration)
+	{
+		std::string exchange, queue;
+
+		if (configuration.exchange.name == "amq") {
+			exchange = "amq." + configuration.exchange.type;
+		}
+
+		if (configuration.exchange.declare) {
+			amqp_exchange_declare(
+				m_connection->m_state,
+				m_id,
+				amqp_cstring_bytes(configuration.exchange.name.c_str()),
+				amqp_cstring_bytes(configuration.exchange.type.c_str()),
+				configuration.exchange.passive,
+				configuration.exchange.durable,
+				configuration.exchange.auto_delete,
+				configuration.exchange.internal,
+				amqp_empty_table);
+
+			die_on_amqp_error(
+				amqp_get_rpc_reply(m_connection->m_state), "exchange.declare");
+
+			exchange = configuration.exchange.name;
+		}
+
+		if (configuration.queue.declare) {
+			amqp_queue_declare_ok_t *r =
+				amqp_queue_declare(
+					m_connection->m_state,
+					m_id,
+					amqp_cstring_bytes(configuration.queue.name.c_str()),
+					configuration.queue.passive,
+					configuration.queue.durable,
+					configuration.queue.exclusive,
+					configuration.queue.auto_delete,
+					amqp_empty_table);
+
+			die_on_amqp_error(
+				amqp_get_rpc_reply(m_connection->m_state), "queue.declare");
+
+			queue = std::string((char*)r->queue.bytes, r->queue.len);
+		}
+
+		if (configuration.queue.bind) {
+			amqp_queue_bind(
+				m_connection->m_state,
+				m_id,
+				amqp_cstring_bytes(queue.c_str()),
+				amqp_cstring_bytes(exchange.c_str()),
+				amqp_cstring_bytes(configuration.routing_key.c_str()),
+				amqp_empty_table);
+
+			die_on_amqp_error(
+				amqp_get_rpc_reply(m_connection->m_state), "queue.bind");
+		}
+
+		return std::make_tuple(exchange, queue);
+	}
+
+	void basic_publish(const std::string &exchange, const std::string &routing_key, const Message &message, bool mandatory, bool immediate)
+	{
+		die_on_error(
+			amqp_basic_publish(
+				m_connection->m_state,
+				m_id,
+				amqp_cstring_bytes(exchange.c_str()),
+				amqp_cstring_bytes(routing_key.c_str()),
+				mandatory,
+				immediate,
+				&message.properties,
+				message.body),
+			"basic.publish");
+	}
+
+	void basic_consume(const std::string &queue_name, const std::string &consumer_tag, bool no_local, bool no_ack, bool exclusive)
+	{
+		amqp_basic_consume(
+			m_connection->m_state,
+			m_id,
+			amqp_cstring_bytes(queue_name.c_str()),
+			amqp_cstring_bytes(consumer_tag.c_str()),
+			no_local,
+			no_ack,
+			exclusive,
+			amqp_empty_table);
+
+		die_on_amqp_error(
+			amqp_get_rpc_reply(m_connection->m_state), "basic.consume");
+	}
+
+	void basic_cancel(const std::string &consumer_tag)
+	{
+		if (!consumer_tag.empty()) {
+			amqp_basic_cancel(m_connection->m_state, m_id, amqp_cstring_bytes(consumer_tag.c_str()));
+			die_on_amqp_error(amqp_get_rpc_reply(m_connection->m_state), "basic.cancel");
+		}
+	}
+
+	void basic_qos(uint32_t prefetch_size, uint16_t prefetch_count, bool global)
+	{
+		if (!amqp_basic_qos(m_connection->m_state, m_id, prefetch_count, prefetch_size, global)) {
+			die_on_amqp_error(amqp_get_rpc_reply(m_connection->m_state), "basic.qos");
+		}
+	}
+
+	void basic_ack(uint64_t delivery_tag, bool multiple)
+	{
+		die_on_error(amqp_basic_ack(m_connection->m_state, m_id, delivery_tag, multiple), "basic.ack");
+	}
+
+	void basic_nack(uint64_t delivery_tag, bool multiple, bool requeue)
+	{
+		die_on_error(amqp_basic_nack(m_connection->m_state, m_id, delivery_tag, multiple, requeue), "basic.nack");
+	}
+};
+
 Connection::Connection(
 	const std::string &host, int port,
 	const std::string &user,
 	const std::string &password,
 	const std::string &vhost, int frame_max
-	)
+	) : m_impl(new ConnectionImpl(host, port, user, password, vhost, frame_max))
 {
-	state = amqp_new_connection();
-	
-	amqp_socket_t *socket = amqp_tcp_socket_new(state);
-	if (!socket) {
-		die("AMQP creating TCP socket failed");
-	}
-
-	int status = amqp_socket_open(socket, host.c_str(), port);
-	if (status) {
-		die("AMQP opening TCP socket on %s:%d failed", host.c_str(), port);
-	}
-
-	die_on_amqp_error(amqp_login(state, vhost.c_str(), 0, frame_max, 0, AMQP_SASL_METHOD_PLAIN,
-			user.c_str(), password.c_str()),
-		"Logging in");
 }
 
 Connection::~Connection()
 {
-	die_on_amqp_error(amqp_connection_close(state, AMQP_REPLY_SUCCESS),
-			"connection.close");
-	die_on_error(amqp_destroy_connection(state), "connection.destroy");
 }
 
-Channel::Channel(Connection *connection) : id(1), connection(connection)
+void Connection::maybe_release_buffers()
 {
-	amqp_channel_open(connection->state, id);
-	die_on_amqp_error(amqp_get_rpc_reply(connection->state),
-			"channel.open");
+	m_impl->maybe_release_buffers();
+}
+
+RpcReply Connection::consume_message(Envelope *envelope, const struct timeval *timeout, int flags)
+{
+	return m_impl->consume_message(envelope, timeout, flags);
+}
+
+Channel::Channel(Connection *connection)
+	: m_impl(new ChannelImpl(connection))
+{
 }
 
 Channel::~Channel()
 {
-	die_on_amqp_error(
-		amqp_channel_close(connection->state, id, AMQP_REPLY_SUCCESS),
-			"channel.close");
+}
+	
+std::tuple<std::string,std::string> Channel::setup(const Configuration &configration)
+{
+	return m_impl->setup(configration);
 }
 
-std::tuple<std::string, std::string> Channel::setup(const Configuration& configuration)
+void Channel::basic_publish(const std::string &exchange, const std::string &routing_key, const Message &message, bool mandatory, bool immediate)
 {
-	std::string exchange, queue;
-
-	if (configuration.exchange.name == "amq") {
-		exchange = "amq." + configuration.exchange.type;
-	}
-
-	if (configuration.exchange.declare) {
-		amqp_exchange_declare(
-			connection->state,
-			id,
-			configuration.exchange.name.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(configuration.exchange.name.c_str()),
-			configuration.exchange.type.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(configuration.exchange.type.c_str()),
-			configuration.exchange.passive,
-			configuration.exchange.durable,
-			configuration.exchange.auto_delete,
-			configuration.exchange.internal,
-			amqp_empty_table);
-
-		die_on_amqp_error(
-			amqp_get_rpc_reply(connection->state), "exchange.declare");
-
-		exchange = configuration.exchange.name;
-	}
-
-	if (configuration.queue.declare) {
-		amqp_queue_declare_ok_t *r =
-			amqp_queue_declare(
-				connection->state,
-				id,
-				configuration.queue.name.empty()
-					? amqp_empty_bytes
-					: amqp_cstring_bytes(configuration.queue.name.c_str()),
-				configuration.queue.passive,
-				configuration.queue.durable,
-				configuration.queue.exclusive,
-				configuration.queue.auto_delete,
-				amqp_empty_table);
-
-		die_on_amqp_error(
-			amqp_get_rpc_reply(connection->state), "queue.declare");
-
-		queue = std::string((char*)r->queue.bytes, r->queue.len);
-	}
-
-	if (configuration.queue.bind) {
-		amqp_queue_bind(
-			connection->state,
-			id,
-			queue.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(queue.c_str()),
-			exchange.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(exchange.c_str()),
-			configuration.routing_key.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(configuration.routing_key.c_str()),
-			amqp_empty_table);
-
-		die_on_amqp_error(
-			amqp_get_rpc_reply(connection->state), "queue.bind");
-	}
-
-	return std::make_tuple(exchange, queue);
+	m_impl->basic_publish(exchange, routing_key, message, mandatory, immediate);
 }
 
-void Channel::publish(const std::string &exchange, const std::string &routing_key, const Message &message, bool mandatory, bool immediate)
+void Channel::basic_consume(const std::string &queue_name, const std::string &consumer_tag, bool no_local, bool no_ack, bool exclusive)
 {
-	die_on_error(
-		amqp_basic_publish(
-			connection->state,
-			id,
-			exchange.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(exchange.c_str()),
-			routing_key.empty()
-				? amqp_empty_bytes
-				: amqp_cstring_bytes(routing_key.c_str()),
-			mandatory,
-			immediate,
-			&message.properties,
-			message.body),
-		"basic.publish");
+	m_impl->basic_consume(queue_name, consumer_tag, no_local, no_ack, exclusive);
 }
 
-void Channel::consume(const std::string &queue_name, struct timeval *tv, std::function<void(const Envelope &)> callback, const std::string &consumer_tag, bool no_local, bool no_ack, bool exclusive)
+void Channel::basic_cancel(const std::string &consumer_tag)
 {
-	amqp_basic_consume(
-		connection->state,
-		id,
-		queue_name.empty()
-			? amqp_empty_bytes
-			: amqp_cstring_bytes(queue_name.c_str()),
-		consumer_tag.empty()
-			? amqp_empty_bytes
-			: amqp_cstring_bytes(consumer_tag.c_str()),
-		no_local,
-		no_ack,
-		exclusive,
-		amqp_empty_table);
-
-	die_on_amqp_error(
-		amqp_get_rpc_reply(connection->state), "basic.consume");
-
-	consumer_loop = true;
-	while (consumer_loop) {
-		amqp_rpc_reply_t res;
-		amqp_envelope_t envelope;
-
-		amqp_maybe_release_buffers(connection->state);
-		res = amqp_consume_message(connection->state, &envelope, tv, 0);
-
-		if (AMQP_RESPONSE_NORMAL != res.reply_type) {
-			continue;
-		}
-
-		callback(envelope);
-
-		amqp_destroy_envelope(&envelope);
-	}
+	m_impl->basic_cancel(consumer_tag);
 }
 
-void Channel::cancel(const std::string &consumer_tag)
+void Channel::basic_qos(uint32_t prefetch_size, uint16_t prefetch_count, bool global)
 {
-	if (!consumer_tag.empty()) {
-		amqp_basic_cancel(connection->state, id, amqp_cstring_bytes(consumer_tag.c_str()));
-		die_on_amqp_error(amqp_get_rpc_reply(connection->state), "basic.cancel");
-	}
+	m_impl->basic_qos(prefetch_size, prefetch_count, global);
 }
 
-void Channel::qos(uint32_t prefetch_size, uint16_t prefetch_count, bool global)
+void Channel::basic_ack(uint64_t delivery_tag, bool multiple)
 {
-	if (!amqp_basic_qos(connection->state, id, prefetch_count, prefetch_size, global)) {
-		die_on_amqp_error(amqp_get_rpc_reply(connection->state), "basic.qos");
-	}
+	m_impl->basic_ack(delivery_tag, multiple);
 }
 
-void Channel::acknowledge(uint64_t delivery_tag, bool multiple)
+void Channel::basic_nack(uint64_t delivery_tag, bool multiple, bool requeue)
 {
-	die_on_error(amqp_basic_ack(connection->state, id, delivery_tag, multiple), "basic.ack");
-}
-
-void Channel::reject(uint64_t delivery_tag, bool multiple, bool requeue)
-{
-	die_on_error(amqp_basic_nack(connection->state, id, delivery_tag, multiple, requeue), "basic.nack");
+	m_impl->basic_nack(delivery_tag, multiple, requeue);
 }
 
 } // end namespace amqp
@@ -245,7 +297,7 @@ MessageBroker::MessageBroker(
 		throw std::runtime_error("vhost is not specified, it is required");
 	}
 	if (port <= 0) {
-		throw std::runtime_error("port is not valid, it must be a positive number");
+		throw std::runtime_error("port is not valm_id, it must be a positive number");
 	}
 
 	m_host = host;
@@ -294,7 +346,7 @@ void MessageBroker::publish(const Configuration &configuration, const std::strin
 	message.setProperty("Content-Type", "application/json");
 	message.setProperty("Delivery-Mode", (uint8_t)2);
 	
-	channel.publish(exchange, configuration.routing_key, message);
+	channel.basic_publish(exchange, configuration.routing_key, message);
 }
 
 MessageBroker::Response::Ptr MessageBroker::publish(const Configuration &configuration, const std::string &messagebody, struct timeval *timeout)
@@ -314,11 +366,26 @@ MessageBroker::Response::Ptr MessageBroker::publish(const Configuration &configu
 
 	Response::Ptr response;
 
-	channel.publish(exchange, configuration.routing_key, request);
-	channel.consume(reply_to, timeout, [&](const auto& envelope) {
+	channel.basic_publish(exchange, configuration.routing_key, request);
+	channel.basic_consume(reply_to);
+
+	for(;;) {
+		RpcReply res;
+		Envelope envelope;
+
+		connection.maybe_release_buffers();
+		res = connection.consume_message(&envelope, timeout, 0);
+
+		if (AMQP_RESPONSE_NORMAL != res.reply_type) {
+			continue;
+		}
+
 		response = std::make_shared<Response>(envelope.message);
-		channel.stop_consuming();
-	});
+
+		amqp_destroy_envelope(&envelope);
+
+		break;
+	}
 
 	return response;
 }
@@ -331,9 +398,23 @@ void MessageBroker::subscribe(const Configuration &configuration, std::function<
 
 		auto[exchange, queue] = channel.setup(configuration);
 
-		channel.consume(queue, nullptr, [&](const auto& envelope) {
+		channel.basic_consume(queue);
+
+		for(;;) {
+			RpcReply res;
+			Envelope envelope;
+
+			connection.maybe_release_buffers();
+			res = connection.consume_message(&envelope, nullptr, 0);
+
+			if (AMQP_RESPONSE_NORMAL != res.reply_type) {
+				continue;
+			}
+
 			callback(envelope.message);
-		});
+
+			amqp_destroy_envelope(&envelope);
+		}
 	});
 
 	worker.detach();
@@ -347,7 +428,19 @@ void MessageBroker::subscribe(const Configuration &configuration, std::function<
 
 		auto[exchange, queue] = channel.setup(configuration);
 
-		channel.consume(queue, nullptr, [&](const auto& envelope){
+		channel.basic_consume(queue);
+
+		for(;;) {
+			RpcReply res;
+			Envelope envelope;
+
+			connection.maybe_release_buffers();
+			res = connection.consume_message(&envelope, nullptr, 0);
+
+			if (AMQP_RESPONSE_NORMAL != res.reply_type) {
+				continue;
+			}
+
 			Request request(envelope.message);
 			Response response;
 
@@ -361,8 +454,10 @@ void MessageBroker::subscribe(const Configuration &configuration, std::function<
 			response.setProperty("Delivery-Mode", (uint8_t)2);
 			response.setProperty("Type", ok ? "response" : "error");
 
-			channel.publish("", reply_to, response);
-		});
+			channel.basic_publish("", reply_to, response);
+
+			amqp_destroy_envelope(&envelope);
+		}
 	});
 
 	worker.detach();
--- message_broker.hpp
+++ message_broker.hpp
@@ -48,88 +48,92 @@ class Message : protected amqp_message_t
 public:
 	typedef std::shared_ptr<Message> Ptr;
 
-	friend class Channel;
-
-	Message() {
+	inline Message()
+	{
 		properties._flags = 0;
 	}
 
-	Message(const std::string &body) : Message() {
+	inline Message(const std::string &body) : Message()
+	{
 		this->body = amqp_bytes_malloc_dup(amqp_cstring_bytes(body.c_str()));
 	}
 
-	Message(const amqp_message_t &message) {
+	inline Message(const amqp_message_t &message)
+	{
 		*this = message;
 	}
 
-	Message& operator=(const amqp_message_t & message) {
-		if (this->body != message.body) {
-			this->body = amqp_bytes_malloc_dup(message.body);
-			this->properties = message.properties;
-			this->pool = message.pool;
-			
-			for (auto iter = propertyMap.begin(); iter != propertyMap.end(); iter++) {
-				switch (iter->second.flag & message.properties._flags) {
-				case AMQP_BASIC_CONTENT_TYPE_FLAG:
-					properties.content_type = amqp_bytes_malloc_dup(message.properties.content_type);
-					break;
-				case AMQP_BASIC_CONTENT_ENCODING_FLAG:
-					properties.content_encoding = amqp_bytes_malloc_dup(message.properties.content_encoding);
-					break;
-				case AMQP_BASIC_DELIVERY_MODE_FLAG:
-					properties.delivery_mode = message.properties.delivery_mode;
-					break;
-				case AMQP_BASIC_PRIORITY_FLAG:
-					properties.priority = message.properties.priority;
-					break;
-				case AMQP_BASIC_CORRELATION_ID_FLAG:
-					properties.correlation_id = amqp_bytes_malloc_dup(message.properties.correlation_id);
-					break;
-				case AMQP_BASIC_REPLY_TO_FLAG:
-					properties.reply_to = amqp_bytes_malloc_dup(message.properties.reply_to);
-					break;
-				case AMQP_BASIC_EXPIRATION_FLAG:
-					properties.expiration = amqp_bytes_malloc_dup(message.properties.expiration);
-					break;
-				case AMQP_BASIC_MESSAGE_ID_FLAG:
-					properties.message_id = amqp_bytes_malloc_dup(message.properties.message_id);
-					break;
-				case AMQP_BASIC_TIMESTAMP_FLAG:
-					properties.timestamp = message.properties.timestamp;
-					break;
-				case AMQP_BASIC_TYPE_FLAG:
-					properties.type = amqp_bytes_malloc_dup(message.properties.type);
-					break;
-				case AMQP_BASIC_USER_ID_FLAG:
-					properties.user_id = amqp_bytes_malloc_dup(message.properties.user_id);
-					break;
-				case AMQP_BASIC_APP_ID_FLAG:
-					properties.app_id = amqp_bytes_malloc_dup(message.properties.app_id);
-					break;
-				case AMQP_BASIC_CLUSTER_ID_FLAG:
-					properties.cluster_id = amqp_bytes_malloc_dup(message.properties.cluster_id);
-					break;
-				}
+	Message& operator=(const amqp_message_t & message)
+	{
+		this->body = amqp_bytes_malloc_dup(message.body);
+		this->properties = message.properties;
+		this->pool = message.pool;
+		
+		for (auto iter = propertyMap.begin(); iter != propertyMap.end(); iter++) {
+			switch (iter->second.flag & message.properties._flags) {
+			case AMQP_BASIC_CONTENT_TYPE_FLAG:
+				properties.content_type = amqp_bytes_malloc_dup(message.properties.content_type);
+				break;
+			case AMQP_BASIC_CONTENT_ENCODING_FLAG:
+				properties.content_encoding = amqp_bytes_malloc_dup(message.properties.content_encoding);
+				break;
+			case AMQP_BASIC_DELIVERY_MODE_FLAG:
+				properties.delivery_mode = message.properties.delivery_mode;
+				break;
+			case AMQP_BASIC_PRIORITY_FLAG:
+				properties.priority = message.properties.priority;
+				break;
+			case AMQP_BASIC_CORRELATION_ID_FLAG:
+				properties.correlation_id = amqp_bytes_malloc_dup(message.properties.correlation_id);
+				break;
+			case AMQP_BASIC_REPLY_TO_FLAG:
+				properties.reply_to = amqp_bytes_malloc_dup(message.properties.reply_to);
+				break;
+			case AMQP_BASIC_EXPIRATION_FLAG:
+				properties.expiration = amqp_bytes_malloc_dup(message.properties.expiration);
+				break;
+			case AMQP_BASIC_MESSAGE_ID_FLAG:
+				properties.message_id = amqp_bytes_malloc_dup(message.properties.message_id);
+				break;
+			case AMQP_BASIC_TIMESTAMP_FLAG:
+				properties.timestamp = message.properties.timestamp;
+				break;
+			case AMQP_BASIC_TYPE_FLAG:
+				properties.type = amqp_bytes_malloc_dup(message.properties.type);
+				break;
+			case AMQP_BASIC_USER_ID_FLAG:
+				properties.user_id = amqp_bytes_malloc_dup(message.properties.user_id);
+				break;
+			case AMQP_BASIC_APP_ID_FLAG:
+				properties.app_id = amqp_bytes_malloc_dup(message.properties.app_id);
+				break;
+			case AMQP_BASIC_CLUSTER_ID_FLAG:
+				properties.cluster_id = amqp_bytes_malloc_dup(message.properties.cluster_id);
+				break;
 			}
 		}
+
 		return *this;
 	}
 
-	~Message() {
+	inline virtual ~Message()
+	{
 		// In rabbitmq-c amqp_destroy_message() frees memory associated 
 		// with a amqp_message_t allocated in amqp_read_message.
 		// Instead, we manually delete fields depending on
 		// how object was created, due to in many case amqp_destroy_envelope() implicitly
 		// calls amqp_destroy_message().
 		for (auto iter = propertyMap.begin(); iter != propertyMap.end(); iter++) {
-			// Skip non amqp_bytes_t types.
-			if (!(iter->second.flag & (
-				AMQP_BASIC_DELIVERY_MODE_FLAG |
-				AMQP_BASIC_PRIORITY_FLAG |
-				AMQP_BASIC_TIMESTAMP_FLAG))) {
-				amqp_bytes_t* ptr = reinterpret_cast<amqp_bytes_t*>(iter->second.ptr);
-				if (ptr->bytes) {
-					amqp_bytes_free(*ptr);
+			if (iter->second.flag & properties._flags) {
+				// Skip non amqp_bytes_t properties.
+				if (!(iter->second.flag & (
+					AMQP_BASIC_DELIVERY_MODE_FLAG |
+					AMQP_BASIC_PRIORITY_FLAG |
+					AMQP_BASIC_TIMESTAMP_FLAG))) {
+					amqp_bytes_t* ptr = reinterpret_cast<amqp_bytes_t*>(iter->second.ptr);
+					if (ptr->bytes) {
+						amqp_bytes_free(*ptr);
+					}
 				}
 			}
 		}
@@ -139,25 +143,53 @@ public:
 		}
 	}
 
-	void setProperty(const std::string & key, const char *value) {
-		*reinterpret_cast<amqp_bytes_t*>(propertyMap[key].ptr) = amqp_cstring_bytes(value);
-		properties._flags |= propertyMap[key].flag;
-	}
-	void setProperty(const std::string & key, uint8_t value) {
-		*reinterpret_cast<uint8_t*>(propertyMap[key].ptr) = value;
-		properties._flags |= propertyMap[key].flag;
-	}
-	void setProperty(const std::string & key, uint64_t value) {
-		*reinterpret_cast<uint64_t*>(propertyMap[key].ptr) = value;
-		properties._flags |= propertyMap[key].flag;
+	inline void setProperty(const std::string & key, const char *value)
+	{
+		if (propertyMap.find(key) == propertyMap.end()) return;
+		if (!(propertyMap[key].flag & (
+			AMQP_BASIC_DELIVERY_MODE_FLAG |
+			AMQP_BASIC_PRIORITY_FLAG |
+			AMQP_BASIC_TIMESTAMP_FLAG))) {
+			*reinterpret_cast<amqp_bytes_t*>(propertyMap[key].ptr) = amqp_bytes_malloc_dup(amqp_cstring_bytes(value));
+			properties._flags |= propertyMap[key].flag;
+		}
 	}
 
-	void setBody(const std::string & body) {
+	inline void setProperty(const std::string & key, uint8_t value)
+	{
+		if (propertyMap.find(key) == propertyMap.end()) return;
+		if (propertyMap[key].flag & (
+			AMQP_BASIC_DELIVERY_MODE_FLAG |
+			AMQP_BASIC_PRIORITY_FLAG |
+			AMQP_BASIC_TIMESTAMP_FLAG)) {
+			*reinterpret_cast<uint8_t*>(propertyMap[key].ptr) = value;
+			properties._flags |= propertyMap[key].flag;
+		}
+	}
+
+	inline void setProperty(const std::string & key, uint64_t value)
+	{
+		if (propertyMap.find(key) == propertyMap.end()) return;
+		if (propertyMap[key].flag & (
+			AMQP_BASIC_DELIVERY_MODE_FLAG |
+			AMQP_BASIC_PRIORITY_FLAG |
+			AMQP_BASIC_TIMESTAMP_FLAG)) {
+			*reinterpret_cast<uint64_t*>(propertyMap[key].ptr) = value;
+			properties._flags |= propertyMap[key].flag;
+		}
+	}
+
+	inline void setBody(const std::string & body)
+	{
 		this->body = amqp_bytes_malloc_dup(amqp_cstring_bytes(body.c_str()));
 	}
-	std::string getBody() const {
+
+	inline std::string getBody() const
+	{
 		return std::string((char *)body.bytes, (int)body.len);
 	}
+
+	friend class ChannelImpl;
 
 protected:
 	struct PropertyDescriptor {
@@ -182,10 +214,8 @@ protected:
 	};
 };
 
-/**
- * @brief A "message envelope" object containing the message body and delivery metadata.
- */
 using Envelope = amqp_envelope_t;
+using RpcReply = amqp_rpc_reply_t;
 
 /**
  * @brief Creates a new connection to an AMQP broker
@@ -193,9 +223,9 @@ using Envelope = amqp_envelope_t;
  */
 class Connection
 {
-public:
-	friend class Channel;
+	std::unique_ptr<class ConnectionImpl> m_impl;
 
+public:
 	/**
 	 * @brief Establish an amqp connection by parameters used to connect to the RabbitMQ broker
 	 * 
@@ -216,8 +246,10 @@ public:
 
 	virtual ~Connection();
 
-protected:
-        std::unique_ptr<class ConnectionImpl> m_impl;
+	void maybe_release_buffers();
+	RpcReply consume_message(Envelope *envelope, const struct timeval *timeout, int flags);
+
+	friend class ChannelImpl;
 };
 
 /**
@@ -226,26 +258,25 @@ protected:
  */
 class Channel
 {
+	std::unique_ptr<class ChannelImpl> m_impl;
+
 public:
-	Channel(Connection &connection);
+	Channel(Connection *connection);
 	virtual ~Channel();
 
 	std::tuple<std::string,std::string> setup(const Configuration &configration);
-	
-	void publish(const std::string &exchange, const std::string &routing_key, const Message &message, bool mandatory = false, bool immediate = false);
 
-	void consume(const std::string &queue_name, struct timeval *timeout, std::function<void(const Envelope &)>, const std::string &consumer_tag = "", bool no_local = false, bool no_ack = true, bool exclusive = false);
+	void basic_publish(const std::string &exchange, const std::string &routing_key, const Message &message, bool mandatory = false, bool immediate = false);
 
-	void cancel(const std::string &consumer_tag);
+	void basic_consume(const std::string &queue_name, const std::string &consumer_tag = "", bool no_local = false, bool no_ack = true, bool exclusive = false);
 
-	void qos(uint32_t prefetch_size, uint16_t prefetch_count, bool global);
+	void basic_cancel(const std::string &consumer_tag);
 
-	void acknowledge(uint64_t delivery_tag, bool multiple = false);
+	void basic_qos(uint32_t prefetch_size, uint16_t prefetch_count, bool global);
 
-	void reject(uint64_t delivery_tag, bool multiple = false, bool requeue = false);
+	void basic_ack(uint64_t delivery_tag, bool multiple = false);
 
-protected:
-	std::unique_ptr<class ChannelImpl> m_impl;
+	void basic_nack(uint64_t delivery_tag, bool multiple = false, bool requeue = false);
 };
 
 /**
